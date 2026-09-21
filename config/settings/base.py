94b17a6ca32f3e7db65799ea928973202b519908
Mustahib <mustahibmusic@gmail.com>
2026-09-22 02:04:16 +0530

    fix(settings): enable django postgres for catalog indexes

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",  # GinIndex / trigram search (apps.catalog)
 
     # Third-party
     "rest_framework",
